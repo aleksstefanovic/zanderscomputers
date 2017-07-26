0f9721819d7e22165c31d919b8cb625ed7c54f35
Aleksandar Stefanovic <aleks@searsinitium.com>
2017-07-25 23:19:28 -0400

    stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,18 @@
 class ItemsController < ApplicationController
     def index
         @searchQuery = params[:search]
-        @items = Item.where("name like ?", "%#{@searchQuery}%")
+        @category = params[:selected_item_type]
+        searchItemType = ItemType.where("name like ?", "%#{@category}%").first
+
+        @items = Item.where("name like ?", "%#{@searchQuery}%").where(:item_type => searchItemType)
+        #if searchItemType != nil
+            #searchItemType = searchItemType.id
+            #@itemsByCategory = Item.where(:item_type => searchItemType)
+            #@items += @itemsByCategory
+        #end 
+
         @items.each do |item|
+            Rails.logger.debug("debugging...: " + @item.inspect)
             item[:item_type] = ItemType.where(:id => item[:item_type]).first.name
         end
     end
