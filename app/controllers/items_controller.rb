1fb13b8206e974a2021bf15b039c35637ec0a74e
Aleksandar Stefanovic <aleks@searsinitium.com>
2017-07-30 15:18:27 -0400

    more css and sorting results

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,16 @@ class ItemsController < ApplicationController
     def index
         @searchQuery = params[:search]
         searchItemType = params[:selected_item_type]
-        #searchItemType = ItemType.where("name like ?", "%#{@category}%").first
 
         @items = Item.where("name like ?", "%#{@searchQuery}%").where(:item_type => searchItemType)
-        #if searchItemType != nil
-            #searchItemType = searchItemType.id
-            #@itemsByCategory = Item.where(:item_type => searchItemType)
-            #@items += @itemsByCategory
-        #end 
+        if searchItemType == nil
+            @items = Item.all.order ('name ASC')
+        end 
 
         @items.each do |item|
             Rails.logger.debug("debugging...: " + @item.inspect)
             item[:item_type] = ItemType.where(:id => item[:item_type]).first.name
+            item[:description] = item[:description][0..50]
         end
     end
     
