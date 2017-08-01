c250eb53aebf312def33bdb76b97f463c1ed15d3
Aleksandar Stefanovic <aleks@searsinitium.com>
2017-08-01 19:26:05 -0400

    with cart msotly working

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,22 +6,16 @@ class CartsController < ApplicationController
         @builds = Build.where(:userid => current_user.id)
         @builds.each do |build|
             build[:description] = build[:description][0..100]
-            cart = Cart.where(:userid => current_user.id)
-            if cart == []
+            cart = Cart.where(:userid => current_user.id).where(:buildid => build[:id]).first
+            if cart == [] or cart == nil
                 next
-            else
-                cart = cart.where(:buildid => build[:id])
-                if cart == []
-                    next
-                end
             end
-            build[:id] = cart[:id]
-            @carts.push(build)
+            @carts.push({ cartid: cart[:id], buildid: build[:id], name: build[:name], description: build[:description] })
         end
     end
 
     def new
-        @existingCart = Cart.where(:buildid => params[:buildId])
+        @existingCart = Cart.where(:buildid => params[:buildId]).first
         if @existingCart == nil
             @cart = Cart.new
             @cart[:userid] = current_user.id
@@ -34,6 +28,7 @@ class CartsController < ApplicationController
     def destroy
         @cart = Cart.find(params[:id])
         @cart.destroy
+        redirect_to carts_path
     end
 
 
